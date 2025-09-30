09ed5ca859f66a981f22947dcda791f47c37f766
Taro Anzai <taro.anzai@gmail.com>
2025-09-30 13:44:04 +0900

    Refactor enviroiment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs';
+import fs from 'fs'
+import process from 'process'
 
-export default defineConfig({
-  base: '/mahjong/',
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync('./ssl/localhost.key'),
-      cert: fs.readFileSync('./ssl/localhost.crt'),
-    },
-    host: 'localhost',
-    port: 5173
+export default defineConfig(({ mode }) => {
+  // 環境変数を読み込む
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base:  '/mahjong/' ,
+    plugins: [react()],
+    server: {
+      host: 'localhost',
+      port: 5173,
+      https: env.VITE_USE_HTTPS === 'true'
+        ? {
+            key: fs.readFileSync('./ssl/localhost.key'),
+            cert: fs.readFileSync('./ssl/localhost.crt'),
+          }
+        : false
+    }
   }
 })
